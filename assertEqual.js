5a87e792445c82269faa85b5b6f6927005b3b927
John Webster <johnrwebs@gmail.com>
2019-07-22 22:30:42 +0000

    add true/false checker file

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const Pass = "✅Assertion Passed: " + actual + " === " + expected;
+  const Fail = "🛑Assertion Failed: " + actual + " !== " + expected;
+  actual === expected ? console.log(Pass) : console.log(Fail);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("test", "test");
+assertEqual(1, 2);
