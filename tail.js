ac17ad7a4bf43800bd09d088a0bcabfae7c7cd6b
John Webster <johnrwebs@gmail.com>
2019-07-22 23:43:07 +0000

    tail exercise and tests

---
+++ tail.js
@@ -0,0 +1,17 @@
+// TEST FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const Pass = `✅Assertion Passed: ${actual} === ${expected}`;
+  const Fail = `🛑Assertion Failed: ${actual} !== ${expected}`;
+  actual === expected ? console.log(Pass) : console.log(Fail);
+};
+
+// ASSIGNMENT CODE
+const tail = function(array) {
+  let modifyArray = [...array];
+  return modifyArray.splice(1);
+};
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
