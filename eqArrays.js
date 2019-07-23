5a0d74b4e795b0c70bec4bb4ab34bf6ce0c12b89
John Webster <johnrwebs@gmail.com>
2019-07-23 23:47:36 +0000

    adding completed eqArrays exercise

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+// assertEqual FUNCTION IMPLEMENTATION
+
+const assertEqual = function(actual, expected) {
+  const Pass = `✅Assertion Passed: ${actual} === ${expected}`;
+  const Fail = `🛑Assertion Failed: ${actual} !== ${expected}`;
+  actual === expected ? console.log(Pass) : console.log(Fail);
+};
+
+// EXERCISE FUNCTION
+
+let eqArrays = function(firstArray, secondArray) {
+  let result = true;
+  for (let i = 0; i < firstArray.length; i++) {
+    result = true;
+    result && firstArray[i] === secondArray[i] ? result = true : result = false;
+  }
+  return result;
+};
+
+// TESTING
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
