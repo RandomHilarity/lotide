7b17fdcfa0adae34c8beaccb2ad4fbbf418eea76
John Webster <johnrwebs@gmail.com>
2019-07-29 19:14:35 +0000

    changed middleTest and tailTest to use Mocha/Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,16 @@
-const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
+const assert = require("chai").assert;
 
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-console.log(assertArraysEqual(middle([1], [])));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+describe("#middle", () => {
+  it("return 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns empty array for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("should return [3, 4] for array [1 , 2, 3, 4, 5, 6", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,14 @@
 const tail = require("../tail.js");
-const assertEqual = require("../assertEqual.js");
+const assert = require("chai").assert;
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-console.log("✅✅ " + assertEqual(tail(words).length, 2)); // no need to capture the return value since we are not checking it
-console.log("✅✅ " + assertEqual(words.length, 3)); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("should return ['Lighthouse', 'Labs'] when given array [`Yo Yo`, `Lighthouse`, `Labs`]", () => {
+    assert.deepEqual(tail([`Yo Yo`, `Lighthouse`, `Labs`]), [`Lighthouse`, `Labs`]);
+  });
+
+  it("should not modify the original array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+});
