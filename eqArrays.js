bab6a610004e068d40fab0b6bba39f0377f882fa
John Webster <johnrwebs@gmail.com>
2019-07-29 16:24:54 +0000

    refactoring eqArrays.js, added test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-// assertEqual FUNCTION IMPLEMENTATION
-
-const assertEqual = function(actual, expected) {
-  const Pass = `✅Assertion Passed: ${actual} === ${expected}`;
-  const Fail = `🛑Assertion Failed: ${actual} !== ${expected}`;
-  actual === expected ? console.log(Pass) : console.log(Fail);
-};
-
-// EXERCISE FUNCTION
-
 let eqArrays = function(firstArray, secondArray) {
   let result = true;
   firstArray.length === secondArray.length ? "" : result = false;
@@ -17,10 +7,4 @@ let eqArrays = function(firstArray, secondArray) {
   return result;
 };
 
-// TESTING
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
